43d87295bc4af26208f884b7c1b13be1e7506f2b
Vaith <vaithwee@yeah.net>
2018-12-30 14:47:24 +0800

    Add wilddcard character example

---
+++ struts-2-wildcard-character-example/java/xyz/vaith/web/action/UserAction.java
@@ -0,0 +1,22 @@
+package xyz.vaith.web.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserAction extends ActionSupport {
+
+    public String add()  {
+        return SUCCESS;
+    }
+
+    public String remove()  {
+        return SUCCESS;
+    }
+
+    public String update()  {
+        return SUCCESS;
+    }
+
+    public String read()  {
+        return SUCCESS;
+    }
+}
---
+++ struts-2-wildcard-character-example/src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h2>wildcard character</h2>
+<a href="/user/add">Add</a> <br />
+<a href="/user/remove">Remove</a> <br />
+<a href="/user/update">Update</a> <br />
+<a href="/user/read">Read</a> <br />
+
+</body>
+</html>
