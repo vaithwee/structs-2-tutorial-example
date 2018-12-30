4660bae26882b7f34c199caef422f1361474a977
Vaith <vaithwee@yeah.net>
2018-12-30 17:33:42 +0800

    Add extends package method example

---
+++ struts-2-user-defined-result-example/java/xyz/vaith/web/action/AnotherCodeAction.java
@@ -0,0 +1,10 @@
+package xyz.vaith.web.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class AnotherCodeAction extends ActionSupport {
+
+    public String code() {
+        return SUCCESS;
+    }
+}
--- struts-2-user-defined-result-example/src/main/webapp/index.jsp
+++ struts-2-user-defined-result-example/src/main/webapp/index.jsp
@@ -2,5 +2,6 @@
 <body>
 <h2>User Defined Result</h2>
 <a href="/code">Code</a>
+<a href="/anotherCode">AnotherCode</a>
 </body>
 </html>
